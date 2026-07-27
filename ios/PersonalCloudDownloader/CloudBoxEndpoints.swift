30efb73a20df3fbf97a2f0169184bd63cd7508fb
amanrai16300-test <amanrai1630@gmail.com>
2026-07-28 04:27:31 +0900

    Bust Downloader WebView cache

--- ios/PersonalCloudDownloader/CloudBoxEndpoints.swift
+++ ios/PersonalCloudDownloader/CloudBoxEndpoints.swift
@@ -6,6 +6,6 @@ enum CloudBoxEndpoints {
     static let fastAPIBaseURL = URL(string: "http://\(serverHost):8000")!
     static let qBittorrentURL = URL(string: "http://\(serverHost):8080")!
     static let filesWebBaseURL = URL(string: "http://\(serverHost):8090")!
-    static let downloaderWebAppURL = URL(string: "\(filesWebBaseURL.absoluteString)/app/")!
+    static let downloaderWebAppURL = URL(string: "\(filesWebBaseURL.absoluteString)/app/?v=phase6-20260728")!
     static let filesURL = URL(string: "\(filesWebBaseURL.absoluteString)/files/")!
 }
