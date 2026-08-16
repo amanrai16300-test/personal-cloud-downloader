b33082a08304d3574e0e6efd4d24832d33e8b149
amanrai16300-test <amanrai1630@gmail.com>
2026-08-17 00:02:51 +0900

    fix: bust Direct Downloads web cache

--- ios/PersonalCloudDownloader/CloudBoxEndpoints.swift
+++ ios/PersonalCloudDownloader/CloudBoxEndpoints.swift
@@ -6,6 +6,6 @@ enum CloudBoxEndpoints {
     static let fastAPIBaseURL = URL(string: "http://\(serverHost):8000")!
     static let qBittorrentURL = URL(string: "http://\(serverHost):8080")!
     static let filesWebBaseURL = URL(string: "http://\(serverHost):8090")!
-    static let downloaderWebAppURL = URL(string: "\(filesWebBaseURL.absoluteString)/app/?v=phase6-20260728")!
+    static let downloaderWebAppURL = URL(string: "\(filesWebBaseURL.absoluteString)/app/?v=direct-downloads-20260816")!
     static let filesURL = URL(string: "\(filesWebBaseURL.absoluteString)/files/")!
 }
